c35338eb33666f31eab5a84489c2f55845434e42
Nhat Tan <trngtn44@gmail.com>
2022-04-16 21:11:28 +0700

    finish login

--- Nodejs/src/server.js
+++ Nodejs/src/server.js
@@ -3,11 +3,21 @@ import bodyParser from "body-parser";
 import viewEngine from "./config/viewEngine";
 import initwebRoutes from "./routes/web";
 import connectDB from "./config/connectDB";
+// import cors from 'cors';
 
 require('dotenv').config();
 //Use to run process.env.PORT
 
 let app = express();
+// app.use(cors({ origin: true }));
+//middleware
+app.use(function (req, res, next) {
+    res.setHeader('Access-Control-Allow-Origin', process.env.URL_REACT);
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+    res.setHeader('Access-Control-Allow-Headers', 'X-Requested-with, content-type');
+    res.setHeader('Access-Control-Allow-Credentials', true);
+    next();
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
