e97407a1064061c82deb41cd862dc12d9a7fea1e
Nhat Tan <trngtn44@gmail.com>
2022-05-03 10:50:51 +0700

    role menu

--- Reactjs/src/containers/Header/Header.js
+++ Reactjs/src/containers/Header/Header.js
@@ -6,6 +6,7 @@ import Navigator from '../../components/Navigator';
 import { adminMenu } from './menuApp';
 import './Header.scss';
 import { LANGUAGES } from '../../utils'
+import { FormattedMessage } from 'react-intl';
 
 class Header extends Component {
 
@@ -13,7 +14,7 @@ class Header extends Component {
         this.props.changeLanguageAppRedux(language)
     }
     render() {
-        const { processLogout, language } = this.props;
+        const { processLogout, language, userInfo } = this.props;
 
         return (
             <div className="header-container">
@@ -22,6 +23,10 @@ class Header extends Component {
                     <Navigator menus={adminMenu} />
                 </div>
                 <div className="languages">
+                    <span className="welcome">
+                        <FormattedMessage id="homeheader.welcome" />
+                        {userInfo && userInfo.firstName ? userInfo.firstName : " "}
+                    </span>
                     <span className={language === LANGUAGES.VI ? "language-vi active" : "language-vi"}
                         onClick={() => this.handleChangeLanguage(LANGUAGES.VI)}>
                         VI
@@ -45,6 +50,7 @@ class Header extends Component {
 const mapStateToProps = state => {
     return {
         isLoggedIn: state.user.isLoggedIn,
+        userInfo: state.user.userInfo,
         language: state.app.language,
     };
 };
